2775bd3d92886b74261068452aff224c48675821
Vishnudharshan1997 <39819806+Vishnudharshan1997@users.noreply.github.com>
2018-07-12 15:09:59 +0600

    Update Largest1.py

--- Largest1.py
+++ Largest1.py
@@ -1,12 +1,12 @@
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
  
-if (num1 > num2) and (num1 > num3):
-   largest = num1
-elif (num2 > num1) and (num2 > num3):
-   largest = num2
+if (a > b) and (a > c):
+   largest = a
+elif (b > a) and (b > c):
+   largest = b
 else:
-   largest = num3
+   largest = c
  
 print(largest)
